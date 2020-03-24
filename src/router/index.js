088fa5df1ed13621fa3d4a67ecb3b5ed505f24cb
nathaly <nathzzpersia@gmail.com>
2020-03-24 11:58:13 -0400

    Routes created

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,50 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventList from '../views/EventList'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'event-list',
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/event/:id',
+    name: 'event-show',
+    props: true,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ '../views/EventShow.vue')
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/EventCreate.vue')
+  },
+  {
+    path: '/user/:username',
+    name: 'user',
+    props: true,
+    component: () => import(/* webpackChunkName: "about" */ '../views/User.vue')
+  },
+  {
+    path: '*',
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/FileNotFound.vue')
   }
-];
+]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
-});
+})
 
-export default router;
+export default router
